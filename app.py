4c980d5438130374c701e34a4ac02044328befdc
ArrudaMen <arrudadourado@hotmail.com>
2026-04-28 22:46:00 -0300

    Fix: ajustando caminhos de importação e dependências

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import os
 from groq import Groq
 from langchain_community.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings import HuggingFaceEmbeddings
 
